a805f60ea21e6994c1cebb50dbe7b2fdc73b6f5d
Geoffrey Hing <ghing@themarshallproject.org>
2024-04-20 20:36:49 -0400

    Initial commit
    Bootstrap site using Eleventy and write initial content.

---
+++ .eleventy.js
@@ -0,0 +1,11 @@
+module.exports = function(eleventyConfig) {
+  eleventyConfig.addPassthroughCopy("css/style.css");
+  eleventyConfig.addPassthroughCopy("android-chrome-192x192.png");
+  eleventyConfig.addPassthroughCopy("android-chrome-512x512.png");
+  eleventyConfig.addPassthroughCopy("apple-touch-icon.png");
+  eleventyConfig.addPassthroughCopy("favicon-16x16.png");
+  eleventyConfig.addPassthroughCopy("favicon-32x32.png");
+  eleventyConfig.addPassthroughCopy("site.webmanifest");
+
+  eleventyConfig.addPassthroughCopy("img");
+};
---
+++ README.md
@@ -0,0 +1,5 @@
+# George Hing's memorial website
+
+This is a memorial website for George Hing built using the [11ty](https://www.11ty.dev/) static site generator.
+
+The site is published to https://www.georgehing.life/
